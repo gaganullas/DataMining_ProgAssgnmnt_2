445c1b21ffe1279541b12f7c0cc5739e2a2bc286
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-03-26 21:56:05 -0400

    Add files via upload

--- part1.py
+++ part1.py
@@ -16,6 +16,13 @@ import math
 from sklearn.cluster import AgglomerativeClustering
 import pickle
 import utils as u
+from sklearn.cluster import KMeans
+from matplotlib.backends.backend_pdf import PdfPages
+
+warnings.filterwarnings("ignore")
+
+
+
 
 
 # ----------------------------------------------------------------------
@@ -29,8 +36,20 @@ In the first task, you will explore how k-Means perform on datasets with diverse
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_kmeans():
-    return None
+def fit_kmeans(dataset, n_clusters):
+    
+    data, labels = dataset
+    
+    # Standardize the data
+    scaler = StandardScaler()
+    data_scaled = scaler.fit_transform(data)
+    
+    # Fit KMeans clustering
+    kmeans = KMeans(n_clusters=n_clusters, init='random', random_state=42)
+    kmeans.fit(data_scaled)
+    
+    # Return predicted labels
+    return kmeans.labels_
 
 
 def compute():
@@ -42,7 +61,27 @@ def compute():
 
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # 'nc', 'nm', 'bvv', 'add', 'b'. keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
+    
     dct = answers["1A: datasets"] = {}
+    
+    n_samples = 100
+    seed = 42
+    
+    nc = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=seed)
+    dct['nc'] = list(nc)
+    nm = datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=seed)
+    dct['nm'] = list(nm)
+    bvv = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=seed)
+    dct['bvv'] = list(bvv)
+    X, y = datasets.make_blobs(n_samples=n_samples, random_state=seed)
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
+    X_aniso = np.dot(X, transformation)
+    add = (X_aniso, y)
+    dct['add'] = list(add)
+    b = datasets.make_blobs(n_samples=n_samples, random_state=seed)
+    dct['b'] = list(b)
+    
+
 
     """
    B. Write a function called fit_kmeans that takes dataset (before any processing on it), i.e., pair of (data, label) Numpy arrays, and the number of clusters as arguments, and returns the predicted labels from k-means clustering. Use the init='random' argument and make sure to standardize the data (see StandardScaler transform), prior to fitting the KMeans estimator. This is the function you will use in the following questions. 
@@ -61,11 +100,62 @@ def compute():
     # dct value: return a dictionary of one or more abbreviated dataset names (zero or more elements) 
     # and associated k-values with correct clusters.  key abbreviations: 'nc', 'nm', 'bvv', 'add', 'b'. 
     # The values are the list of k for which there is success. Only return datasets where the list of cluster size k is non-empty.
-    dct = answers["1C: cluster successes"] = {"xy": [3,4], "zx": [2]} 
+        
+    given_datasets = {
+        "nc": nc,
+        "nm": nm,
+        "bvv": bvv,
+        "add": add,
+        "b": b
+        }
+        
+    num_clusters = [2, 3, 5, 10]
+    dataset_keys = ['nc', 'nm', 'bvv', 'add', 'b']
+    pdf_filename = "report_1C.pdf"
+    pdf_pages = []
+    
+    cluster_successes = {}
+    cluster_failures = []
+    
+    fig, axes = plt.subplots(len(num_clusters), len(dataset_keys), figsize=(20, 16))
+    fig.suptitle('Scatter plots for different datasets and number of clusters', fontsize=16)
+    
+    for j, dataset_key in enumerate(dataset_keys):
+        data, labels = given_datasets[dataset_key]
+
+        for i, k in enumerate(num_clusters):
+            predicted_labels = fit_kmeans(given_datasets[dataset_key], n_clusters=k)
+
+            ax = axes[i, j]
+            ax.scatter(data[:, 0], data[:, 1], c=predicted_labels, cmap='viridis')
+            ax.set_title(f'{dataset_key}, k={k}')
+
+
+            if len(set(predicted_labels)) == len(set(labels)):
+                if dataset_key in cluster_successes:
+                    cluster_successes[dataset_key].append(k)
+                else:
+                    cluster_successes[dataset_key] = [k]
+            else:
+                cluster_failures.append(dataset_key)
+
+
+    plt.tight_layout()
+    pdf_pages.append(fig)
+    plt.close(fig)
+
+
+    with PdfPages(pdf_filename) as pdf:
+        for page in pdf_pages:
+            pdf.savefig(page)
+
+    #dct = answers["1C: cluster successes"] = {"xy": [3,4], "zx": [2]} 
+    dct = answers["1C: cluster successes"] = {"bvv": [3], "add": [3],"b":[3]} 
 
     # dct value: return a list of 0 or more dataset abbreviations (list has zero or more elements, 
     # which are abbreviated dataset names as strings)
-    dct = answers["1C: cluster failures"] = ["xy"]
+    # dct = answers["1C: cluster failures"] = ["xy"]
+    dct = answers["1C: cluster failures"] = {"nc","nm"} 
 
     """
     D. Repeat 1.C a few times and comment on which (if any) datasets seem to be sensitive to the choice of initialization for the k=2,3 cases. You do not need to add the additional plots to your report.
--- part2.py
+++ part2.py
@@ -24,6 +24,8 @@ import math
 from sklearn.cluster import AgglomerativeClustering
 import pickle
 import utils as u
+from sklearn.cluster import KMeans
+warnings.filterwarnings("ignore")
 
 # ----------------------------------------------------------------------
 """
@@ -36,8 +38,23 @@ In this task you will explore different methods to find a good value for k
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_kmeans():
-    return None
+def fit_kmeans(dataset, n_clusters):
+    
+    X, labels, true_centers = dataset
+  
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    
+    kmeans = KMeans(n_clusters=n_clusters, init='random', random_state=42)
+    kmeans.fit(X_scaled)
+    
+    centers = kmeans.cluster_centers_
+    
+    distances = np.sqrt(np.sum((X_scaled - centers[kmeans.labels_])**2, axis=1))
+    
+    sse = np.sum(distances ** 2)
+    
+    return sse
 
 
 
@@ -48,9 +65,17 @@ def compute():
     """
     A.	Call the make_blobs function with following parameters :(center_box=(-20,20), n_samples=20, centers=5, random_state=12).
     """
+    
+    centers = 5
+    n_samples = 20
+    random_state = 12
+    center_box = (-20, 20)
+    X, y,returned_center = make_blobs(n_samples=n_samples, centers=centers, center_box=center_box, random_state=random_state,return_centers=True)
 
     # dct: return value from the make_blobs function in sklearn, expressed as a list of three numpy arrays
-    dct = answers["2A: blob"] = [np.zeros(0)]
+    #dct = answers["2A: blob"] = [np.zeros(0)]
+    
+    dct = answers["2A: blob"] = [X, y, returned_center]
 
     """
     B. Modify the fit_kmeans function to return the SSE (see Equations 8.1 and 8.2 in the book).
@@ -62,20 +87,47 @@ def compute():
     """
     C.	Plot the SSE as a function of k for k=1,2,….,8, and choose the optimal k based on the elbow method.
     """
-
+    data = make_blobs(n_samples=20, centers=5, center_box=(-20, 20), random_state=12,return_centers=True)
+    
     # dct value: a list of tuples, e.g., [[0, 100.], [1, 200.]]
-    # Each tuple is a (k, SSE) pair
-    dct = answers["2C: SSE plot"] = [[0.0, 100.0]]
+    # Each tuple is a (k, SSE) pair   
+    sse_values = []
+    for k in range(1, 8 + 1):
+        sse = fit_kmeans(data, k)
+        sse_values.append((k, sse))
+
+    sse_values_formatted = [[k, float(sse)] for k, sse in sse_values]
+
+    dct = answers["2C: SSE plot"] = sse_values_formatted
+        
+    #dct = answers["2C: SSE plot"] = [[0.0, 100.0]]
 
     """
     D.	Repeat part 2.C for inertia (note this is an attribute in the kmeans estimator called _inertia). Do the optimal k’s agree?
     """
+    
+    k_values = list(range(1, 9))
+    
+    X, labels, true_centers = make_blobs(n_samples=n_samples, centers=centers, center_box=center_box, random_state=random_state,return_centers=True)
+    
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    inertia_values = []
+    for k in k_values:
+        kmeans = KMeans(n_clusters=k, init='random', random_state=42)
+        kmeans.fit(X_scaled)  
+        inertia_values.append((k,kmeans.inertia_))
+    
+    sse_values_formatted = [[k, float(sse)] for k, sse in inertia_values]
+
+    #answers["2D: SSE plot"] = sse_values_formatted
 
     # dct value has the same structure as in 2C
-    dct = answers["2D: inertia plot"] = [[0.0, 100.0]]
+    dct = answers["2D: inertia plot"] = sse_values_formatted
 
     # dct value should be a string, e.g., "yes" or "no"
-    dct = answers["2D: do ks agree?"] = ""
+    dct = answers["2D: do ks agree?"] = "yes"
 
     return answers
 
