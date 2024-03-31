9ab5d48b68730e31eeea3823849929484b7d3633
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-03-31 18:38:22 -0400

    Add files via upload

--- part1.py
+++ part1.py
@@ -222,7 +222,7 @@ def compute():
     # dct value: list of dataset abbreviations
     # Look at your plots, and return your answers.
     # The plot is part of your report, a pdf file name "report.pdf", in your repository.
-    dct = answers["1D: datasets sensitive to initialization"] = ["nc","nm"]
+    dct = answers["1D: datasets sensitive to initialization"] = ["nc"]
 
     return answers
 
